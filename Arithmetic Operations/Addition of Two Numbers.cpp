4b11741bb44800c2d87eb2e0550e5500d7acac4a
Gautam Dhiman <gautamdhiman8708311909@gmail.com>
2026-01-27 06:42:54 +0530

    Uncomment code for addition of two numbers

--- Arithmetic Operations/Addition of Two Numbers.cpp
+++ Arithmetic Operations/Addition of Two Numbers.cpp
@@ -1,11 +1,11 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	int a,b,result;
-//	cout<<"Enter Two Numbers:";
-//	cin>>a>>b;
-//	result=a+b;
-//	cout<<"The Addition of Two Numbers Are:"<<result;
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,result;
+	cout<<"Enter Two Numbers:";
+	cin>>a>>b;
+	result=a+b;
+	cout<<"The Addition of Two Numbers Are:"<<result;
+	return 0;
+}
