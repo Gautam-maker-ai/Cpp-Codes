0a2d1d3a1806d780882a1a359c1f8479acfa638c
Gautam Dhiman <gautamdhiman8708311909@gmail.com>
2026-01-27 06:37:06 +0530

    Added Subtraction programs

---
+++ Arithmetic Operations/Subtraction of Two Numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int result,a,b;
+	cout<<"Enter Two Numbers:";
+	cin>>a>>b;
+	result=a-b;
+	cout<<"The Subtraction of Two Numbers is:"<<result;
+	return 0;
+}
