cb6e0e4a8761ff778939a4fd79de519c05db62a5
Gautam Dhiman <gautamdhiman8708311909@gmail.com>
2026-01-28 05:27:29 +0530

    Multiplication of Two Numbers

---
+++ Arithmetic Operations/Multiplication of Two Numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,result;
+	cout<<"Enter Two Numbers:";
+	cin>>a>>b;
+	result=a*b;
+	cout<<"Multiplication is:"<<result;
+	return 0;
+}
--- Skeleton of C++.cpp
+++ Skeleton of C++.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"Hello! Guys Hope You All Are Well";
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+////int main()
+//{
+//	cout<<"Hello! Guys Hope You All Are Well";
+//	return 0;
+//}
