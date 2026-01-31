308cc9e385b86820fad36f80b77a87fe1bfec894
Gautam Dhiman <gautamdhiman8708311909@gmail.com>
2026-01-31 21:30:22 +0530

    Added Maximum Numbers

---
+++ Conditional Statements/If-else/Maximum of two numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter Two Numbers:";
+	cin>>a>>b;
+	if(a>b)
+	{
+		cout<<"A is Maximum";
+	}
+	else
+	{
+		cout<<"B is Maximum";
+	}
+	return 0;
+}
