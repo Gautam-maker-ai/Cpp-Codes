d380f57261fbcd6164cd1f197b5ef39e7bfb6837
Gautam Dhiman <gautamdhiman8708311909@gmail.com>
2026-02-02 19:30:57 +0530

    Updated code

--- Arithmetic Operations/Conversion from kilometer to m,cmand mm.cpp
+++ Arithmetic Operations/Conversion from kilometer to m,cmand mm.cpp
@@ -6,7 +6,7 @@ int main()
 	float Km;
 	cout<<"Enter Distance in KM:";
 	cin>>Km;
-	cout<<fixed<<setprecision(0);//setprecision() is used to control how many digits are shown in the output of floating-point numbers.
+    cout<<fixed<<setprecision(0);//setprecision() is used to control how many digits are shown in the output of floating-point numbers.
 	cout<<"In Meteres:"<<Km*1000<<endl;
 	cout<<"In Centieteres:"<<Km*100000<<endl;
 	cout<<"In Millimeteres:"<<Km*1000000<<endl;
