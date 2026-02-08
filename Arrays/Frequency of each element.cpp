9fb11a4d0a8706ad397dcd7e15d95d52e34796c6
Gautam Dhiman <gautamdhiman8708311909@gmail.com>
2026-02-08 09:00:35 +0530

    Added Frequency of each element

---
+++ Arrays/Frequency of each element.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter n:";
+	cin>>n;
+	int A[n];
+	cout<<"Enter Elements:";
+	for(int i=0;i<n;i++)
+	{
+		cin>>A[i];
+	}
+	map<int,int> freq;
+	for(int i=0;i<n;i++)
+	{
+		freq[A[i]]++;
+	}
+	for(auto x:freq)
+	{
+		cout<<x.first<<"=>"<<x.second<<endl;
+	}
+	return 0;
+}
