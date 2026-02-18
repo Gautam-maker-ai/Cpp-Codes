7410faf9c5aecf58d852786aacf6d6f12111bc6d
Gautam Dhiman <gautamdhiman8708311909@gmail.com>
2026-02-19 00:47:58 +0530

    Added Student Marks

---
+++ Oops/Accessors and Mutators/Student Marks.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+	private: int marks;
+	public:
+		   void setMarks(int m)
+		   {
+		   	if(m>=0 && m<=100)
+		   	{
+		   		marks=m;
+			}
+			else
+			{
+				cout<<"Marks can not be displayed"<<endl;
+			}
+		   }
+		   int getMarks()
+		   {
+		   	return marks;
+		   }
+};
+int main()
+{
+  Student s;
+  s.setMarks(1);
+  cout<<"Marks Are:"<<s.getMarks()<<endl;
+  return 0;	
+}
